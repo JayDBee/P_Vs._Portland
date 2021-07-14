ead1a775a61d154b493ac42a8804c24b14d60c9f
Daniel <jbecerra@pdx.edu>
2021-07-14 16:39:48 -0700

    support added for dynamic websites

--- scripts/example_request.py
+++ scripts/example_request.py
@@ -1,12 +1,49 @@
-import requests
-from selenium import webdriver
+#import requests
 from bs4 import BeautifulSoup
+import asyncio
+from pyppeteer import launch
+#import os
+
+URL = "https://www.portlandmercury.com/blogtown/2021/07/13/35028680/most-heatwave-victims-died-alone-with-no-air-conditioning-county-report-finds"
+#page = requests.get(URL)
+
+async def main():
+    browser = await launch()
+    page = await browser.newPage()
+
+    await page.goto(URL)
+    page_content = await page.content()
+
+    #soup
+    soup = BeautifulSoup(page_content, "html.parser")
+    print(soup.prettify())
+
+    #close browser
+    await browser.close()
+
+asyncio.get_event_loop().run_until_complete(main())
+
+#should only print pg. txt for confirmation
+#print(page.text)
+#print(page.content
+print("\nshould be empty above^^")
 
 
-URL = "https://realpython.github.io/fake-jobs/"
-page = requests.get(URL)
 
-print(page.text)
+#in page, search for 
+#Entire article @ <div id="articleComponent-#####" class="component">
+#Headline @ <h1 class="headline">...</h1>
+#IMG @ <div class="blogImageCenter img-responsive" style>...</div>
+#Text @ <div class = "article-text category-blogtown">
+
+#now that we have the page source, get the article txt
+#print txt
+
+#save the text
+#END
+
+
+
 
 
 #const int LIMIT;
