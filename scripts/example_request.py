6d2eb02a93c5dd72dbf7ada9ea4703b8ed8ccc35
Daniel <jbecerra@pdx.edu>
2021-07-14 15:07:46 -0700

    Added to the list of sources for data & added repos for web scraping

--- scripts/example_request.py
+++ scripts/example_request.py
@@ -1,6 +1,15 @@
 import requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
 
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
 
 print(page.text)
+
+
+#const int LIMIT;
+#FRESHNESS
+#if data has been "LIVE" for longer than LIMIT, then take off
+#should always be "running"
