399158aca63d57604e646b6ce806922d7dbe9a84
Daniel <jbecerra@pdx.edu>
2021-07-14 20:28:15 -0700

    finished example script for the EveroutPortland

--- scripts/example_request.py
+++ scripts/example_request.py
@@ -1,12 +1,17 @@
-#import requests
 from bs4 import BeautifulSoup
 import asyncio
 from pyppeteer import launch
-#import os
 
-URL = "https://www.portlandmercury.com/blogtown/2021/07/13/35028680/most-heatwave-victims-died-alone-with-no-air-conditioning-county-report-finds"
+#command line array passed?
+
+
+URL = "https://everout.com/portland/events/live-at-the-lot-liv-warfield-presented-by-portland-radio-project/e102492/"
+
+#Format of this code is found in the link below:
+#https://scrapingant.com/blog/scrape-dynamic-website-with-python
 
 async def main():
+    #launch browser
     browser = await launch()
     page = await browser.newPage()
 
@@ -15,35 +20,12 @@ async def main():
 
     #soup
     soup = BeautifulSoup(page_content, "html.parser")
-    results = soup.find(class_="article-text category-blogtown")
-    print(results.prettify())
+    results = soup.find(class_ = 'description')
+
+    #display
+    print(results.get_text())
+
+    #close ends
     await browser.close()
 
 asyncio.get_event_loop().run_until_complete(main())
-
-#should only print pg. txt for confirmation
-#print(page.text)
-#print(page.content
-
-
-
-#in page, search for 
-#Entire article @ <div id="articleComponent-#####" class="component">
-#Headline @ <h1 class="headline">...</h1>
-#IMG @ <div class="blogImageCenter img-responsive" style>...</div>
-#Text @ <div class = "article-text category-blogtown">
-
-#now that we have the page source, get the article txt
-#print txt
-
-#save the text
-#END
-
-
-
-
-
-#const int LIMIT;
-#FRESHNESS
-#if data has been "LIVE" for longer than LIMIT, then take off
-#should always be "running"
