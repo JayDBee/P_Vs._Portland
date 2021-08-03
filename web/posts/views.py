373dcde43af83c2699e8e7b3fb7216b4091c98a7
Daniel <jbecerra@pdx.edu>
2021-08-03 14:16:19 -0700

    added web page for pages or functions not yet added

--- web/posts/views.py
+++ web/posts/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
+
+def comingSoon(request):
+    return render(request, 'comingSoon.html')
--- web/web/settings.py
+++ web/web/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+STATIC_URL = '/static/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
